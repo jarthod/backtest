d60db8b10c3adcb2a349d665e039fc388e704946
Adrien Jarthon <jobs@adrienjarthon.com>
2013-10-14 21:38:26 +0200

    circular buffer implementation

---
+++ circular_buffer.hpp
@@ -0,0 +1,40 @@
+#include <string>
+
+using namespace std;
+
+template <class T>
+class CircularBuffer
+{
+  public:
+
+    CircularBuffer(unsigned size = 100)
+    {
+      this->size = size;
+      this->next_slot = 0;
+      buffer = new T[size];
+    };
+
+    ~CircularBuffer()
+    {
+      delete[] buffer;
+    };
+
+    void  insert(const T& value)
+    {
+      buffer[next_slot] = value;
+      ++next_slot;
+      if (next_slot >= size)
+        next_slot = 0;
+    };
+
+    const T&  get(unsigned lookback = 0) const
+    {
+
+    };
+
+  private:
+
+    T*        buffer;
+    unsigned  next_slot;
+    unsigned  size;
+};
--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-#include <stdio>
+#include <iostream>
 #include <stdlib.h>
 #include <string.h>
 #include "tick.hpp"
+#include "circular_buffer.hpp"
 
 int main(int argc, char* argv[])
 {
-	FILE* 		file = fopen("EURUSD.csv", "r");
+	FILE* 		file = fopen("data/EURUSD.csv", "r");
 	char 			buffer[200];
 	Tick			tick;
+	CircularBuffer<Tick>	ticks;
 	unsigned	line = 0;
 
 	while(fgets(buffer, 200, file) != NULL)
@@ -20,6 +22,7 @@ int main(int argc, char* argv[])
 		if (line % 1000000 == 0)
 			std::cout << tick.date << std::endl;
 		line++;
+		ticks.insert(tick);
 	}
 
 	fclose(file);
